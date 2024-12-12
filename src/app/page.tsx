72c7b202fa52a87fa6c55266b53d61e2eb80ee32
Ângelo Gabriel <angelogabrielsicssu@gmail.com>
2024-12-12 18:07:55 -0400

    feat: welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,46 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <></>
+    <div>
+
+      <div 
+        className="relative bg-cover bg-center text-white flex flex-row justify-between items-center py-5 px-20"
+        style={{
+          backgroundImage: "url('/backgrounds/welcome.jpg')",
+          height: "550px"
+        }}
+      >
+        
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col text-7xl uppercase gap-2">
+            <h1>Seja</h1>
+            <h1 
+            className="uppercase text-transparent stroke-text"
+            style={{
+              WebkitTextStroke: "2px white",
+              color: "transparent"
+            }}
+            >
+              Bem Vindo</h1>
+          </div>
+
+          <nav className="flex flex-row w-48 border-4 p-2 rounded-lg items-center gap-2 bg-neutral-400 bg-opacity-50 cursor-pointer transition ease-in-out delay-150 hover:scale-105 hover:bg-blue-950 duration-200">
+            <a><Image src={'/icons/circled-play.svg'} width={30} height={30} alt=""></Image></a>
+            <a className="uppercase">Nossa Igreja</a>
+          </nav>
+
+        </div>
+        <div className="flex flex-col items-center gap-5">
+          <h1 className="uppercase text-4xl">Título do Vídeo</h1>
+          <div className="border-8 border-white rounded-md">
+            <iframe src="https://www.youtube.com/embed/MuzUA2ZmxDE" width={570} height={320} className="rounded-md" allowFullScreen/>
+          </div>
+        </div>
+        
+      </div>
+      <span className="h-10 bg-blue-600">NIBGC</span>
+
+    </div>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,18 +5,27 @@ export default function Header() {
     <div className="bg-gradient-to-r from-blue-900 to-blue-800 p-3 flex justify-between items-center">
       <Image src={'/logo-nib.svg'} width={90} height={90} alt={""}></Image>
       <div className="flex gap-12 uppercase text-white items-center">
-        <div className="flex flex-row gap-8">
+        <nav className="flex flex-row gap-8">
           <div className="flex gap-8">
-            <a className="cursor-pointer">Quem Somos</a>
-            <a className="cursor-pointer">Meditação</a>
-            <a className="cursor-pointer">Eventos</a>
+            <a href="#" className="relative cursor-pointer group text-zinc-400 hover:text-white transition ease-in-out duration-300">
+              Quem Somos
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-pink-600 transition-all duration-500 group-hover:w-full rounded-md"></span>
+            </a>
+            <a href="#" className="relative cursor-pointer group text-zinc-400 hover:text-white transition ease-in-out duration-300">
+              Meditação
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-pink-600 transition-all duration-500 group-hover:w-full rounded-md"></span>
+            </a>
+            <a href="#" className="relative cursor-pointer group text-zinc-400 hover:text-white transition ease-in-out duration-300">
+              Eventos
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-pink-600 transition-all duration-500 group-hover:w-full rounded-md"></span>
+            </a>
           </div>
           <div className="flex gap-5" >
-            <a className="cursor-pointer"><Image src={'/icons/youtube.svg'} width={25} height={25} alt={""}></Image></a>
-            <a className="cursor-pointer"><Image src={'/icons/instagram.svg'} width={25} height={25} alt={""}></Image></a>
-            <a className="cursor-pointer"><Image src={'/icons/spotify.svg'} width={25} height={25} alt={""}></Image></a>
+            <a href="#" className="cursor-pointer"><Image src={'/icons/youtube.svg'} width={25} height={25} alt={""}></Image></a>
+            <a href="#" className="cursor-pointer"><Image src={'/icons/instagram.svg'} width={25} height={25} alt={""}></Image></a>
+            <a href="#" className="cursor-pointer"><Image src={'/icons/spotify.svg'} width={25} height={25} alt={""}></Image></a>
           </div>
-        </div>
+        </nav>
         <div className="py-1 px-2 rounded-lg cursor-pointer transition ease-in-out delay-150 bg-orange-500 hover:scale-105 hover:bg-indigo-700 duration-200">
           <a>Contato</a>
         </div>
